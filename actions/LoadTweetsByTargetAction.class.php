e5de1f2f16c1ea9121802da561337f60af0d047f
Gaël PORT <gael.port@rbs.fr>
2010-08-10 15:24:44 +0200

    [FIX] Do not log exception when there is no website in the twitter panel.

--- actions/LoadTweetsByTargetAction.class.php
+++ actions/LoadTweetsByTargetAction.class.php
@@ -12,12 +12,17 @@ class twitterconnect_LoadTweetsByTargetAction extends f_action_BaseJSONAction
 	public function _execute($context, $request)
 	{
 		$result = array();
-		
-		$module = $request->getParameter('currentModule');
-		$startIndex = $request->hasParameter('startIndex') ? $request->getParameter('startIndex') : 0;
-		$pageSize = $request->getParameter('pageSize');
-		$result = twitterconnect_ModuleService::getInstance()->getInfosByDocumentId($request->getParameter('relatedId'), $module, $startIndex, $pageSize);
-		
+		try 
+		{
+			$module = $request->getParameter('currentModule');
+			$startIndex = $request->hasParameter('startIndex') ? $request->getParameter('startIndex') : 0;
+			$pageSize = $request->getParameter('pageSize');
+			$result = twitterconnect_ModuleService::getInstance()->getInfosByDocumentId($request->getParameter('relatedId'), $module, $startIndex, $pageSize);
+		}
+		catch (Exception $e)
+		{
+			return $this->sendJSONException($e);
+		}
 		return $this->sendJSON($result);
 	}
 }
